5c7dfe89ee4f7f0da06917dc16dbb6098618d106
Shlomi Vaknin <shlomi.vaknin@intel.com>
2014-10-09 13:30:38 -0700

    fixed deps

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojure-hadoop/clojure-hadoop "1.4.4"]
-                 [org.netpreserve.commons/webarchive-commons "1.1.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1" ]
+                 [clojure-hadoop/clojure-hadoop "1.4.4"
+                  :exclusions [clojure-complete commons-httpclient org.codehaus.jackson/jackson-core-asl]]
+                 [org.netpreserve.commons/webarchive-commons "1.1.4"
+                  :exclusions [org.apache.hadoop/hadoop-core commons-lang commons-logging log4j commons-configuration]]
+                 ]
   :java-source-paths ["java"]
   :javac-options  ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :aot :all
-  )
+  :aot :all)
